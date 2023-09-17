90664e2ee78d8295c25d67040864e6d0e8a61501
bkfitx <treybertram06@gmail.com>
2023-09-17 10:55:12 -0700

    Initial Commit

---
+++ todoList/todoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  todoListApp.swift
+//  todoList
+//
+//  Created by Trey Bertram on 2023-09-17.
+//
+
+import SwiftUI
+
+@main
+struct todoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
